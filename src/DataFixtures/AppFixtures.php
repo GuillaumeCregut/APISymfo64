0d16b74d07050cd3f5e677fe3539c50e6520f34e
Guillaume Crégut <gcregut@free.fr>
2024-07-30 14:08:37 +0200

    Add user management

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,30 +3,44 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
+use App\Entity\User;
 use App\Entity\Author;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $userPassworHasher)
+    {
+    }
     public function load(ObjectManager $manager): void
     {
         $listAuthor = [];
-        for($i=0;$i<10;$i++) {
+        for ($i = 0; $i < 10; $i++) {
             $author = new Author;
             $author->setFirstname('Prenom' . $i);
             $author->setLastname('Nom' . $i);
             $manager->persist($author);
-            $listAuthor[]=$author;
+            $listAuthor[] = $author;
         }
         for ($i = 0; $i < 20; $i++) {
-            $book= new Book();
+            $book = new Book();
             $book->setTitle('Book ' . $i);
             $book->setCoverText('Cover Text ' . $i);
             $book->setAuthor($listAuthor[array_rand($listAuthor)]);
             $manager->persist($book);
         }
-
+        $userAdmin = new User();
+        $userAdmin->setEmail('admin@example.com');
+        $userAdmin->setRoles(['ROLE_ADMIN']);
+        $userAdmin->setPassword($this->userPassworHasher->hashPassword($userAdmin, 'password'));
+        $manager->persist($userAdmin);
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->userPassworHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
         $manager->flush();
     }
 }
